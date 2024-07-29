ec5f1f4eba2f13ab81b7e7f270cc2dc1728a3fb4
Ilyas Andika <andikailyas01@gmail.com>
2024-07-29 09:28:14 +0700

    fix: change is_published data type

--- db/migrations/20240729015200_create_table_article.up.sql
+++ db/migrations/20240729015200_create_table_article.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE articles (
     user_id INT NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
-    is_published ENUM('true', 'false') DEFAULT 'false',
+    is_published bool DEFAULT false,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id)
